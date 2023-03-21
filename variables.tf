4f941f2626bf54e7f165f3b904a78cd6b6c5ca84
ZConverter-samples <85214181+ZConverter-samples@users.noreply.github.com>
2023-03-21 16:18:55 +0900

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,3 +1,12 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "3.33.0"
+    }
+  }
+}
+
 variable "region" {
   type    = string
   default = null
@@ -7,9 +16,8 @@ variable "vm_name" {
   type = string
 }
 
-variable "is_create_rg" {
-  type    = bool
-  default = false
+variable "vm_password" {
+  type = string
 }
 
 variable "resource_group_name" {
@@ -17,46 +25,7 @@ variable "resource_group_name" {
   default = null
 }
 
-variable "is_create_VN" {
-  type = bool
-}
-
-variable "AVN_name" {
-  type = string
-}
-
-variable "AVN_address_space" {
-  type    = list(string)
-  default = ["10.0.0.0/16"]
-}
-
-variable "is_create_subnet" {
-  type = bool
-}
-
-variable "subnet_name" {
-  type = string
-}
-
-variable "subnet_address_prefixes" {
-  type    = list(string)
-  default = ["10.0.1.0/24"]
-}
-
-variable "nic_name" {
-  type = string
-}
-
-variable "vm_size" {
-  type = string
-}
-
-variable "user_data_file" {
-  type    = string
-  default = null
-}
-
-variable "user_data" {
+variable "create_resource_group_name" {
   type    = string
   default = null
 }
@@ -69,18 +38,57 @@ variable "OS_version" {
   type = string
 }
 
-variable "volume" {
+variable "azure_virtual_network_name" {
+  type    = string
+  default = null
+}
+
+variable "create_azure_virtual_network_name" {
+  type    = string
+  default = null
+}
+
+variable "azure_virtual_network_address_space" {
+  type    = list(string)
+  default = ["10.0.0.0/16"]
+}
+
+variable "subnet_name" {
+  type    = string
+  default = null
+}
+
+variable "create_subnet_name" {
+  type    = string
+  default = null
+}
+
+variable "subnet_address_prefixes" {
+  type    = list(string)
+  default = ["10.0.1.0/24"]
+}
+
+variable "vm_size" {
+  type = string
+}
+
+variable "user_data_file_path" {
+  type    = string
+  default = null
+}
+
+variable "additional_volumes" {
   type    = list(number)
   default = []
 }
 
 variable "create_security_group_rules" {
   type = list(object({
-    direction         = string
-    protocol          = string
-    port_range_min    = string
-    port_range_max    = string
-    remote_ip_prefix  = string
+    direction        = string
+    protocol         = string
+    port_range_min   = string
+    port_range_max   = string
+    remote_ip_prefix = string
   }))
   default = []
 }
